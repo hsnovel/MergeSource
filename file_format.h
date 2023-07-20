64937c17647802c0d0e768d6964949d3e8f3acd7
xcatalyst <cagankorkmaz35@gmail.com>
2023-07-20 23:12:04 +0300

    gonna keep ident maybe for later, can also delete don't know

--- file_format.h
+++ file_format.h
@@ -53,17 +53,6 @@ static struct {
 	unsigned char	ident[16];
 	char*	type[10];
 	char*	isa[79];
-	uint32_t	version;
-	uint32_t	entry;
-	uint32_t	phoff;
-	uint32_t	shoff;
-	uint32_t	flags;
-	uint16_t	ehsize;
-	uint16_t	phentsize;
-	uint16_t	phnum;
-	uint16_t	shentsize;
-	uint16_t	shnum;
-	uint16_t	shstrndx;
 } elf_map_str = {
 
 	/* File type */
